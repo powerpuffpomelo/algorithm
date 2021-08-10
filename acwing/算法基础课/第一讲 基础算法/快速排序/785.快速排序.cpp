0ac0ccf63269da812b475ab92664596c47f8ea1c
mello <powerpuffpomelo@gmail.com>
2021-08-10 16:47:44 +0800

    update

--- acwing/算法基础课/第一讲 基础算法/快速排序/785.快速排序.cpp
+++ acwing/算法基础课/第一讲 基础算法/快速排序/785.快速排序.cpp
@@ -7,14 +7,28 @@ int q[N];
 
 void quick_sort(int q[], int l, int r){
     if(l >= r) return;
-    int i = l - 1, j = r + 1, x = q[l + r >> 1];
-    while(l < r){
+    int i = l - 1, j = r + 1, x = q[l];
+    while(i < j){
         do i++; while(q[i] < x);
         do j--; while(q[j] > x);
-        if(l < r) swap(q[i], q[j]);
+        if(i < j) swap(q[i], q[j]);
     }
     quick_sort(q, l, j);
     quick_sort(q, j+1, r);
+}
+
+void quick_sort_v2(int q[], int l, int r){
+    if (l >= r) return;
+    int i = l, j = r, x = q[l];
+    while (i < j){
+        while (i < j && q[j] > x) j--;
+        q[i] = q[j];
+        while (i < j && q[i] <= x) i++;
+        q[j] = q[i];
+    }
+    q[i] = x;
+    quick_sort(q, l, i - 1);
+    quick_sort(q, i + 1, r);
 }
 
 int main(){
--- acwing/算法基础课/第一讲 基础算法/快速排序/786.第k个数.cpp
+++ acwing/算法基础课/第一讲 基础算法/快速排序/786.第k个数.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 using namespace std;
 
-const int N=100010;
+const int N = 1e6 + 10;
 int n,k;
 int q[N];
 
 int quick_sort(int q[], int l, int r, int k){
     if(l >= r) return q[l];
-    int i = l - 1, j = r + 1, x = q[l + r >> 1];
+    int i = l - 1, j = r + 1, x = q[l];
     while(i<j){
-        do i++; while(q[i] < x);
+        do i++; while(q[i] < x);  //或者写成 while (q[++i] < x);
         do j--; while(q[j] > x);
         if(i<j) swap(q[i], q[j]);
     }
-    if(j - l + 1 >= k) return quick_sort(q, l, j, k);
-    else return quick_sort(q, j+1, r, k - (j - l + 1));
+    int sl = j - l + 1;     //左侧序列的长度
+    if(sl >= k) return quick_sort(q, l, j, k);
+    else return quick_sort(q, j+1, r, k - sl);
 }
 
 int main(){
