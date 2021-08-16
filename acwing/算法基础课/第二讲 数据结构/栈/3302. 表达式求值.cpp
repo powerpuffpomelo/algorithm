60aa7e025761c18c88621cbaac383a31fbf1411d
mello <powerpuffpomelo@gmail.com>
2021-08-16 17:19:12 +0800

    update

--- acwing/算法基础课/第二讲 数据结构/栈/3302. 表达式求值.cpp
+++ acwing/算法基础课/第二讲 数据结构/栈/3302. 表达式求值.cpp
@@ -1,44 +1,46 @@
 #include <iostream>
-#include <stack>
-#include <algorithm>
-#include <unordered_map>
 #include <cstring>
+#include <algorithm>
+#include <stack>
+#include <unordered_map>
 using namespace std;
 
-string str;
 stack<char> op;
 stack<int> num;
+string str;
+unordered_map<char, int> pr{{'+', 1}, {'-', 1}, {'*', 2}, {'/', 2}}; //定义优先级
 
 void eval(){
     int b = num.top(); num.pop();
     int a = num.top(); num.pop();
     char c = op.top(); op.pop();
-    if(c == '+') num.push(a + b);
-    else if(c == '-') num.push(a - b);
-    else if(c == '*') num.push(a * b);
-    else if(c == '/') num.push(a / b);
+    int x;
+    if(c == '+') x = a + b;
+    else if(c == '-') x = a - b;
+    else if(c == '*') x = a * b;
+    else x = a / b;
+    num.push(x);
 }
 
 int main(){
-    ios::sync_with_stdio(false);
-    unordered_map<char, int> pr{{'+', 1}, {'-', 1}, {'*', 2}, {'/', 2}};  //定义优先级
     cin >> str;
     for(int i = 0; i < str.size(); i++){
-        if(isdigit(str[i])){
-            int j = i, x = 0;
-            while(isdigit(str[j]) && j < str.size()){
+        char c = str[i];
+        if(isdigit(c)){
+            int x = 0, j = i;
+            while(j < str.size() && isdigit(str[j])){
                 x = x * 10 + str[j++] - '0';
             }
-            num.push(x);
             i = j - 1;
-        }else if(str[i] == '('){
-            op.push(str[i]);
-        }else if(str[i] == ')'){
+            num.push(x);
+        }else if(c == '('){
+            op.push(c);
+        }else if(c == ')'){
             while(op.top() != '(') eval();
             op.pop();
         }else{
-            while(op.size() && op.top() != '(' && pr[op.top()] >= pr[str[i]]) eval();
-            op.push(str[i]);
+            while(op.size() && op.top() != '(' && pr[op.top()] >= pr[c]) eval();
+            op.push(c);
         }
     }
     while(op.size()) eval();
