25cea0201213fff01d811235366bec11e5d6e6bc
mello <powerpuffpomelo@gmail.com>
2021-08-17 08:40:59 +0800

    update

---
+++ acwing/算法基础课/第二讲 数据结构/单调栈/830.单调栈.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+const int N = 1e6 + 10;
+int st[N], top = -1;
+int n;
+
+int main(){
+    cin >> n;
+    while(n--){
+        int x;
+        cin >> x;
+        while(top != -1 && st[top] >= x) top--;
+        if(top == -1) cout << -1 << ' ';
+        else cout << st[top] << ' ';
+        st[++top] = x;
+    }
+    return 0;
+}
