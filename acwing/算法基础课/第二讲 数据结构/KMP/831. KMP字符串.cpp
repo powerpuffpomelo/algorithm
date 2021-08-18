259ebcb952f8eb7be31131ca0da4726ed522ac05
mello <powerpuffpomelo@gmail.com>
2021-08-18 16:28:43 +0800

    update

---
+++ acwing/算法基础课/第二讲 数据结构/KMP/831. KMP字符串.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+const int N = 1e5 + 10, M = 1e6 + 10;
+int n, m;
+char S[M], P[N];
+int ne[N];
+
+int main(){
+    cin >> n >> P + 1 >> m >> S + 1;
+    
+    // 求next数组
+    for(int i = 2, j = 0; i <= n; i++){
+        while(j && P[i] != P[j + 1]) j = ne[j];
+        if(P[i] == P[j + 1]) j++;
+        ne[i] = j;
+    }
+    
+    // 匹配，复杂度O(2m)
+    for(int i = 1, j = 0; i <= m; i++){
+        while(j && S[i] != P[j + 1]) j = ne[j];
+        if(S[i] == P[j + 1]) j++;
+        if(j == n){
+            cout << i - n << ' ';
+            j = ne[j];
+        }
+    }
+    return 0;
+}
