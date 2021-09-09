4377b10919a9ae4bcc0f28917c8f10595bad63e4
mello <powerpuffpomelo@gmail.com>
2021-09-09 23:36:29 +0800

    update

---
+++ acwing/算法基础课/第二讲 数据结构/堆/838.堆排序.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int N = 1e5 + 10;
+int n, m;
+int h[N], cnt;  // h[N] 存放堆，cnt 是堆的元素数（不同于n）
+
+void down(int i){  // 一路向下到该呆的位置
+    int t = i;
+    if(i * 2 <= cnt && h[i * 2] < h[t]) t = i * 2;
+    if(i * 2 + 1 <= cnt && h[i * 2 + 1] < h[t]) t = i * 2 + 1;    // 与左右子节点比较
+    if(i != t){
+        swap(h[i], h[t]);
+        down(t);
+    }
+}
+
+int main(){
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++) cin >> h[i];
+    cnt = n;
+    for(int i = n / 2; i; i--) down(i);
+    while(m--){
+        cout << h[1] << ' ';
+        h[1] = h[cnt--];
+        down(1);
+    }
+    return 0;
+}
