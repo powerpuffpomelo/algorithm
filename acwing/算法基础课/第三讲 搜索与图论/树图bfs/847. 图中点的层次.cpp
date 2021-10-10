bbc30f017f160f1926b73af388d3e1d4a10b5d94
mello <powerpuffpomelo@gmail.com>
2021-10-10 18:44:13 +0800

    update

--- acwing/算法基础课/第三讲 搜索与图论/树图bfs/847. 图中点的层次.cpp
+++ acwing/算法基础课/第三讲 搜索与图论/树图bfs/847. 图中点的层次.cpp
@@ -1,3 +1,5 @@
+// ###################################################### 版本1 ###################################################### //
+
 #include <iostream>
 #include <cstring>
 #include <queue>
@@ -41,5 +43,52 @@ int main(){
         add(a, b);
     }
     cout << bfs(1) << endl;
+    return 0;
+}
+
+// ###################################################### 版本2 ###################################################### //
+
+#include <iostream>
+#include <cstring>
+#include <queue>
+using namespace std;
+
+const int N = 1e5 + 10, M = N * 2;
+int h[N], e[M], ne[M], idx;
+int d[N];
+int n, m;
+
+void add(int a, int b){
+    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
+}
+
+int bfs(){
+    queue<int> q;
+    q.push(1);
+    d[1] = 0;
+    while(q.size()){
+        auto t = q.front();
+        q.pop();
+        if(t == n) return d[t];
+        for(int i = h[t]; i != -1; i = ne[i]){
+            int j = e[i];
+            if(d[j] != -1) continue;
+            q.push(j);
+            d[j] = d[t] + 1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    memset(h, -1, sizeof(h));
+    memset(d, -1, sizeof(d));
+    scanf("%d%d", &n, &m);
+    while(m--){
+        int a, b;
+        scanf("%d%d", &a, &b);
+        add(a, b);
+    }
+    cout << bfs() << endl;
     return 0;
 }
