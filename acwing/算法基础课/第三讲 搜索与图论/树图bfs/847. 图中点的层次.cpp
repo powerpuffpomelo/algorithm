f2aeaa7381932fe4e45534c69252906fd41b8a06
mello <powerpuffpomelo@gmail.com>
2021-10-10 17:05:53 +0800

    update

--- acwing/算法基础课/第三讲 搜索与图论/树图bfs/847. 图中点的层次.cpp
+++ acwing/算法基础课/第三讲 搜索与图论/树图bfs/847. 图中点的层次.cpp
@@ -1,4 +1,45 @@
-//水水
-//水水
-//water
-//water
+#include <iostream>
+#include <cstring>
+#include <queue>
+using namespace std;
+
+typedef pair<int, int> PII;
+const int N = 1e5 + 10, M = N * 2;
+int n, m;
+int h[N], e[M], ne[M], idx;
+int vis[N];
+
+void add(int a, int b){
+    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
+}
+
+int bfs(int start){
+    queue<PII> q;
+    q.push({start, 0});
+    vis[start] = 1;
+    while(q.size()){
+        auto t = q.front();
+        q.pop();
+        int id = t.first, dis = t.second;
+        if(id == n) return dis;
+        for(int i = h[id]; i != -1; i = ne[i]){
+            int j = e[i];
+            if(vis[j]) continue;
+            vis[j] = 1;
+            q.push({j, dis + 1});
+        }
+    }
+    return -1;
+}
+
+int main(){
+    memset(h, -1, sizeof(h));
+    cin >> n >> m;
+    while(m--){
+        int a, b;
+        cin >> a >> b;
+        add(a, b);
+    }
+    cout << bfs(1) << endl;
+    return 0;
+}
