816ece2a9b3f5f191f0bc74273455295e0c20112
mello <powerpuffpomelo@gmail.com>
2021-11-05 14:21:10 +0800

    update

--- PAT/甲级/1003.cpp
+++ PAT/甲级/1003.cpp
@@ -202,3 +202,93 @@ int main(){
     }
     return 0;
 }
+
+
+// ###################################################### 版本4 ###################################################### //
+// bellman + dfs
+
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+using namespace std;
+
+const int N = 510, INF = 0x3fffffff;
+struct node{
+    int v, d;
+    node(int _v, int _d): v(_v), d(_d) {}
+};
+vector<node> adj[N];
+int w[N], dist[N], num[N];
+set<int> pre[N];
+int n, m, st, ed;
+vector<int> temp_path;
+int ans_weight;
+
+bool bellman(int st){
+    fill(dist, dist + N, INF);
+    dist[st] = 0;
+    num[st] = 1;
+    for(int i = 0; i < n - 1; i++){
+        for(int u = 0; u < n; u++){
+            for(int j = 0; j < adj[u].size(); j++){
+                int v = adj[u][j].v, d = adj[u][j].d;
+                if(dist[v] > dist[u] + d){
+                    dist[v] = dist[u] + d;
+                    pre[v].clear();
+                    pre[v].insert(u);
+                    num[v] = num[u];
+                }else if(dist[v] == dist[u] + d){
+                    pre[v].insert(u);
+                    num[v] = 0;
+                    for(set<int>::iterator it = pre[v].begin(); it != pre[v].end(); it++){
+                        num[v] += num[*it];
+                    }
+                }
+            }
+        }
+    }
+    for(int u = 0; u < n; u++){
+        for(int j = 0; j < adj[u].size(); j++){
+            int v = adj[u][j].v, d = adj[u][j].d;
+            if(dist[v] > dist[u] + d) return false;
+        }
+    }
+    return true;
+}
+
+void dfs(int u){
+    if(u == st){
+        temp_path.push_back(u);   // 递归边界里面单独push_back、pop_back
+        int temp_weight = 0;
+        for(int i = 0; i < temp_path.size(); i++){
+            temp_weight += w[temp_path[i]];
+        }
+        if(temp_weight > ans_weight) ans_weight = temp_weight;
+        temp_path.pop_back();
+        return;
+    }
+    temp_path.push_back(u);
+    for(set<int>::iterator it = pre[u].begin(); it != pre[u].end(); it++){
+        dfs(*it);
+    }
+    temp_path.pop_back();
+}
+
+int main(){
+    cin >> n >> m >> st >> ed;
+    for(int i = 0; i < n; i++){
+        cin >> w[i];
+    }
+    while(m--){
+        int u, v, l;
+        cin >> u >> v >> l;
+        adj[u].push_back(node(v, l));
+        adj[v].push_back(node(u, l));
+    }
+    if(bellman(st)){
+        dfs(ed);
+    }
+    cout << num[ed] << ' ' << ans_weight << endl;
+    return 0;
+}
