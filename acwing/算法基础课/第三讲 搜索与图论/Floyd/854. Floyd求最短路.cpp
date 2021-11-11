5803f8dd9eba5b555a7ebf5bf7fa690b1a9f1450
mello <powerpuffpomelo@gmail.com>
2021-11-11 20:11:38 +0800

    update

---
+++ acwing/算法基础课/第三讲 搜索与图论/Floyd/854. Floyd求最短路.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int N = 210, INF = 0x3fffffff;
+int n, m, Q;
+int d[N][N];
+
+void floyd(){
+    for(int k = 1; k <= n; k++){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(d[i][j] > d[i][k] + d[k][j]){
+                    d[i][j] = d[i][k] + d[k][j];
+                }
+            }
+        }
+    }
+}
+
+int main(){
+    cin >> n >> m >> Q;
+    fill(d[0], d[0] + N * N, INF);
+    for(int i = 1; i <= n; i++){
+        d[i][i] = 0;
+    }
+    while(m--){
+        int x, y, z;
+        cin >> x >> y >> z;
+        d[x][y] = min(d[x][y], z);
+    }
+    floyd();
+    while(Q--){
+        int x, y;
+        cin >> x >> y;
+        if(d[x][y] > INF / 2) puts("impossible");    // 存在负权边，可能有INF更新INF的情况
+        else cout << d[x][y] << endl;
+    }
+    return 0;
+}
