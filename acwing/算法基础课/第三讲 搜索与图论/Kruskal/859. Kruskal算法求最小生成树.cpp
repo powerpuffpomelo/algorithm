deafed7c386af97116200699a89be051a0dfff4d
mello <powerpuffpomelo@gmail.com>
2021-11-15 14:03:37 +0800

    update

---
+++ acwing/算法基础课/第三讲 搜索与图论/Kruskal/859. Kruskal算法求最小生成树.cpp
@@ -0,0 +1,96 @@
+// ###################################################### 版本1 ###################################################### //
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int N = 1e5 + 10, M = N * 2, INF = 0x3fffffff;
+struct edge{
+    int u, v, w;
+    bool operator<(const edge &E)const{
+        return w < E.w;
+    }
+}edges[M];
+int p[N];
+int n, m;
+
+int root(int u){
+    if(p[u] != u) p[u] = root(p[u]);
+    return p[u];
+}
+
+int kruskal(){
+    sort(edges, edges + m);
+    for(int i = 1; i <= n; i++) p[i] = i;
+    int ans = 0, cnt = 0;
+    for(int i = 0; i < m; i++){
+        edge ee = edges[i];
+        int u = ee.u, v = ee.v, w = ee.w;
+        int ru = root(u), rv = root(v);
+        if(ru != rv){
+            p[ru] = rv;
+            ans += w;
+            cnt += 1;
+        }
+    }
+    if(cnt < n - 1) return INF;
+    return ans;
+}
+
+int main(){
+    cin >> n >> m;
+    for(int i = 0; i < m; i++){
+        int u, v, w;
+        cin >> u >> v >> w;
+        edges[i] = {u, v, w};
+    }
+    int ans = kruskal();
+    if(ans == INF) cout << "impossible" << endl;
+    else cout << ans << endl;
+    return 0;
+}
+
+// ###################################################### 版本2 ###################################################### //
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int N = 1e5 + 10, M = 2 * N;
+struct edge{
+    int u, v, w;
+}edges[M];
+int n, m, ans;
+int p[N];
+int num_edge;
+
+bool cmp(edge a, edge b){
+    return a.w < b.w;
+}
+
+int root(int u){
+    if(p[u] == u) return p[u];
+    return p[u] = root(p[u]);
+}
+
+int main(){
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) p[i] = i;
+    for(int i = 0; i < m; i++){
+        int u, v, w;
+        cin >> u >> v >> w;
+        edges[i] = {u, v, w};
+    }
+    sort(edges, edges + m, cmp);
+    for(int i = 0; i < m; i++){
+        edge ee = edges[i];
+        int u = ee.u, v = ee.v;
+        if(root(u) == root(v)) continue;
+        p[root(u)] = root(v);
+        ans += ee.w;
+        num_edge += 1;
+        if(num_edge == n - 1) break;
+    }
+    if(num_edge == n - 1) cout << ans << endl;
+    else cout << "impossible" << endl;
+    return 0;
+}
