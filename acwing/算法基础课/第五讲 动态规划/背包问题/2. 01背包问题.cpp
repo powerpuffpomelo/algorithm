8dff7a4766fb5449ae85726ecd0afb9dff9d42e2
mello <powerpuffpomelo@gmail.com>
2021-11-24 00:50:51 +0800

    update

--- acwing/算法基础课/第五讲 动态规划/背包问题/2. 01背包问题.cpp
+++ acwing/算法基础课/第五讲 动态规划/背包问题/2. 01背包问题.cpp
@@ -1,3 +1,5 @@
+// ###################################################### 版本1 ###################################################### //
+
 #include <iostream>
 #include <algorithm>
 using namespace std;
@@ -5,7 +7,7 @@ using namespace std;
 const int N = 1010;
 int w[N], v[N];    // w 重量（体积）；v 价值
 int dp[N];         // dp[i][j]代表 前i件物品放入体积不超j的背包的最大价值，一维是二维的省略版
-int n, m;
+int n, m;          // m 背包容量
 
 int main(){
     cin >> n >> m;
@@ -18,5 +20,31 @@ int main(){
         }
     }
     cout << dp[m] << endl;
+    return 0;
+}
+
+// ###################################################### 版本2 ###################################################### //
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int N = 1010;
+int w[N], v[N];    // w 重量（体积）；v 价值
+int dp[N][N];      // dp[i][j]代表 前i件物品放入体积不超j的背包的最大价值，一维是二维的省略版
+int n, m;          // m 背包容量
+
+int main(){
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++){
+        cin >> w[i] >> v[i];
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            dp[i][j] = dp[i -  1][j];
+            if(j >= w[i]) dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+        }
+    }
+    cout << dp[n][m] << endl;
     return 0;
 }
