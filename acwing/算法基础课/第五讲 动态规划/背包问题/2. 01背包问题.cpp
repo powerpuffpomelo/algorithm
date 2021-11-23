4d5d79685eb2f9218150376e2b69c7b2a6433e1d
mello <powerpuffpomelo@gmail.com>
2021-11-24 00:46:00 +0800

    update

--- acwing/算法基础课/第五讲 动态规划/背包问题/2. 01背包问题.cpp
+++ acwing/算法基础课/第五讲 动态规划/背包问题/2. 01背包问题.cpp
@@ -3,24 +3,20 @@
 using namespace std;
 
 const int N = 1010;
-int w[N], v[N];
-int dp[N];
-int n, W;
+int w[N], v[N];    // w 重量（体积）；v 价值
+int dp[N];         // dp[i][j]代表 前i件物品放入体积不超j的背包的最大价值，一维是二维的省略版
+int n, m;
 
 int main(){
-    cin >> n >> W;
+    cin >> n >> m;
     for(int i = 1; i <= n; i++){
         cin >> w[i] >> v[i];
     }
     for(int i = 1; i <= n; i++){
-        for(int j = W; j >= w[i]; j--){
+        for(int j = m; j >= w[i]; j--){
             dp[j] = max(dp[j], dp[j - w[i]] + v[i]);
         }
     }
-    int ans = 0;
-    for(int i = 1; i <= W; i++){
-        if(dp[i] > ans) ans = dp[i];
-    }
-    cout << ans << endl;
+    cout << dp[m] << endl;
     return 0;
 }
