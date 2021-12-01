98004ea14da6ec69a676714ad4d8f1b8a602397f
mello <powerpuffpomelo@gmail.com>
2021-12-02 00:25:08 +0800

    update

--- acwing/算法基础课/第二讲 数据结构/单调队列/154. 滑动窗口.cpp
+++ acwing/算法基础课/第二讲 数据结构/单调队列/154. 滑动窗口.cpp
@@ -1,3 +1,5 @@
+// 单调队列
+
 #include <iostream>
 using namespace std;
 
@@ -21,8 +23,8 @@ int main(){
     init();
     for(int i = 0; i < n; i++){
         if(!empty() && q[front + 1] < i - k + 1) front++;  //判断队首元素是否在窗口范围内
-        while(!empty() && arr[q[rear]] >= arr[i]) rear--;
-        q[++rear] = i;
+        while(!empty() && arr[q[rear]] >= arr[i]) rear--;  //本题优化的关键思路：如果新进入窗口的元素比窗口内已有元素x要小，那么x永远无法输出，可以直接丢弃x
+        q[++rear] = i;  // 本题关键思路之二：队列内存放的是索引，而非数值大小。这样才能知道小数值出窗口了没有
         if(i >= k - 1) cout << arr[q[front + 1]] << ' ';
     }
     cout << endl;
