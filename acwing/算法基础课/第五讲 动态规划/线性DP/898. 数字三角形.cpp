60b3b76c54f3b6f39551dfd3427f9471f0d292b3
mello <powerpuffpomelo@gmail.com>
2021-12-04 11:19:16 +0800

    update

---
+++ acwing/算法基础课/第五讲 动态规划/线性DP/898. 数字三角形.cpp
@@ -0,0 +1,62 @@
+// ###################################################### 版本1 ###################################################### //
+// 自底向上
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int N = 510;
+int g[N][N], dp[N][N];
+int n;
+
+int main(){
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cin >> g[i][j];
+        }
+    }
+    for(int j = 1; j <= n; j++){
+        dp[n][j] = g[n][j];
+    }
+    for(int i = n - 1; i >= 1; i--){
+        for(int j = 1; j <= i; j++){
+            dp[i][j] = max(dp[i + 1][j], dp[i + 1][j + 1]) + g[i][j];
+        }
+    }
+    cout << dp[1][1] << endl;
+    return 0;
+}
+
+// ###################################################### 版本2 ###################################################### //
+// 自顶向下
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int N = 510, INF = -1e10;
+int g[N][N], dp[N][N];
+int n;
+
+int main(){
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cin >> g[i][j];
+        }
+    }
+    for(int i = 1; i < n; i++){
+        for(int j = 0; j <= n + 1; j++){
+            dp[i][j] = INF;
+        }
+    }
+    dp[1][1] = g[1][1];
+    for(int i = 2; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + g[i][j];
+        }
+    }
+    int ans = INF;
+    for(int j = 1; j <= n; j++) ans = max(ans, dp[n][j]);
+    cout << ans << endl;
+    return 0;
+}
