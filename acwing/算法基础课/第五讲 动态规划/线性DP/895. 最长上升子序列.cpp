c2d1c14549f4e2ceb5011e3c721700aa499264d3
mello <powerpuffpomelo@gmail.com>
2021-12-05 13:05:24 +0800

    update

---
+++ acwing/算法基础课/第五讲 动态规划/线性DP/895. 最长上升子序列.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int N = 1010;
+int a[N], dp[N];
+int n, ans = 1;
+
+int main(){
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        cin >> a[i];
+        dp[i] = 1;
+        for(int j = 1; j < i; j++){
+            if(a[i] > a[j]){
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+        if(dp[i] > ans) ans = dp[i];
+    }
+    cout << ans << endl;
+    return 0;
+}
