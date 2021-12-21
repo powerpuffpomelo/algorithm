2e531eea09f83ded917bb917dec3f6c444c8bb65
mello <powerpuffpomelo@gmail.com>
2021-12-21 16:41:51 +0800

    update

--- acwing/周赛/第30场周赛/4198. 最长合法括号子串.cpp
+++ acwing/周赛/第30场周赛/4198. 最长合法括号子串.cpp
@@ -10,13 +10,13 @@ int ans = 0, ans_num = 1;
 int main(){
     cin >> str;
     for(int i = 0; str[i]; i++){
-        if(stk.size() && str[i] == ')' && str[stk.top()] == '(') stk.pop();
+        if(stk.size() && str[i] == ')' && str[stk.top()] == '(') stk.pop();  // 消掉
         else stk.push(i);
-        int r;
+        int r;          // 以i为结尾的最长合法子串长度
         if(stk.size()) r = i - stk.top();
         else r = i + 1;
         if(r > ans) ans = r, ans_num = 1;
-        else if(r > 0 && r == ans) ans_num++;
+        else if(r > 0 && r == ans) ans_num++;  // 记得 r > 0
     }
     cout << ans << ' ' << ans_num << endl;
     return 0;
