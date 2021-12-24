63a9d92a152dcdf066fbb3a3e6c10cab756e6d69
mello <powerpuffpomelo@gmail.com>
2021-12-24 23:51:20 +0800

    update

--- leetcode/hot100/72.编辑距离.cpp
+++ leetcode/hot100/72.编辑距离.cpp
@@ -1,12 +1,10 @@
 class Solution {
-    int dp[510][510];
-
 public:
     int minDistance(string word1, string word2) {
-        //23.25
         int l1 = word1.size(), l2 = word2.size();
-        for(int i = 1; i <= l1; i++) dp[i][0] = i;
-        for(int j = 1; j <= l2; j++) dp[0][j] = j;
+        int dp[l1 + 1][l2 + 1];
+        for(int i = 0; i <= l1; i++) dp[i][0] = i;     // 注意dp[0][0]的初始化
+        for(int j = 0; j <= l2; j++) dp[0][j] = j;
         for(int i = 1; i <= l1; i++){
             for(int j = 1; j <= l2; j++){
                 dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1);
