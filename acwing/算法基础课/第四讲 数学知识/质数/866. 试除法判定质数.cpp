5f7e71e93ae1b8f27cd5fc6870ea86d73876ae0c
mello <powerpuffpomelo@gmail.com>
2022-01-03 11:13:19 +0800

    update

--- acwing/算法基础课/第四讲 数学知识/质数/866. 试除法判定质数.cpp
+++ acwing/算法基础课/第四讲 数学知识/质数/866. 试除法判定质数.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
-#include <algorithm>
-#include <cmath>
 using namespace std;
 
 int n;
 
 bool is_prime(int x){
     if(x < 2) return false;
-    for(int i = 2; i <= sqrt(x); i++){
+    for(int i = 2; i <= x / i; i++){   // 不会溢出的写法。不推荐 i * i <= x，会溢出；不推荐 i <= sqrt(x)，每次都要算一遍
         if(x % i == 0) return false;
     }
     return true;
---
+++ acwing/算法基础课/第四讲 数学知识/质数/867. 分解质因数.cpp
@@ -0,0 +1,59 @@
+// ###################################################### 版本1 ###################################################### //
+// 任何大于等于2的正整数都可以表示成质数的乘积
+// n中最多只包含一个大于sqrt(n)的质因子
+#include <iostream>
+using namespace std;
+
+int n;
+
+void divide(int x){
+    for(int i = 2; i <= x / i; i++){
+        if(x % i == 0){
+            int s = 0;
+            while(x % i == 0){
+                s++;
+                x /= i;
+            }
+            printf("%d %d\n", i, s);
+        }
+    }
+    if(x > 1) printf("%d %d\n", x, 1);
+    puts("");
+}
+
+int main(){
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        int a;
+        scanf("%d", &a);
+        divide(a);
+    }
+    return 0;
+}
+
+// ###################################################### 版本2 ###################################################### //
+#include <iostream>
+using namespace std;
+
+int n;
+
+int main(){
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        int a, s;
+        scanf("%d\n", &a);
+        for(int i = 2; i <= a / i; i++){
+            s = 0;
+            while(a % i == 0){
+                s++;
+                a /= i;
+            }
+            if(s){
+                printf("%d %d\n", i, s);
+            }
+        }
+        if(a > 1) printf("%d %d\n", a, 1);
+        puts("");
+    }
+    return 0;
+}
