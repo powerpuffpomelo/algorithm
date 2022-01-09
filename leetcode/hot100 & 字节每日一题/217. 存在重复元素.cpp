eb6ea8313a8a6bc74af2599e30e0ec2fce8c1a56
mello <powerpuffpomelo@gmail.com>
2022-01-09 12:38:33 +0800

    update

--- leetcode/hot100 & 字节每日一题/217. 存在重复元素.cpp
+++ leetcode/hot100 & 字节每日一题/217. 存在重复元素.cpp
@@ -1,5 +1,5 @@
 // ###################################################### 版本1 ###################################################### //
-// 我的初版
+// 我的初版，哈希表
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
@@ -12,3 +12,29 @@ public:
     }
 };
 
+// ###################################################### 版本2 ###################################################### //
+// 排序
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size() - 1; i++){
+            if(nums[i] == nums[i + 1]) return true;
+        }
+        return false;
+    }
+};
+
+// ###################################################### 版本3 ###################################################### //
+// 集合
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for(int x : nums){
+            if(s.find(x) != s.end()) return true;
+            s.insert(x);
+        }
+        return false;
+    }
+};
