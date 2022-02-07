c3c78921c747deb996f671866b23fb487307e1dd
mello <powerpuffpomelo@gmail.com>
2022-02-07 16:41:21 +0800

    update

---
+++ acwing/算法笔试面试辅导课/第五讲/1536. 均分纸牌.cpp
@@ -0,0 +1,20 @@
+// 贪心
+// x代表，a[i + 1]应该转移给a[i]多少
+// 情况合法，如果有负数，只要修改顺序即可。
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 110;
+int n, a[N], ans, sum;
+
+int main(){
+    cin >> n;
+    for(int i = 0; i < n; i++) cin >> a[i], sum += a[i];
+    int avg = sum / n;
+    for(int i = 0, x = 0; i < n; i++){
+        x += a[i] - avg;
+        if(x) ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
