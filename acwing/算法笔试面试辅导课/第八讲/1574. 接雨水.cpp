cafebb8bbfaa92ce5b8ad3cdfa6ab199ebb88bf8
mello <powerpuffpomelo@gmail.com>
2022-02-11 11:40:15 +0800

    update

--- acwing/算法笔试面试辅导课/第八讲/1574. 接雨水.cpp
+++ acwing/算法笔试面试辅导课/第八讲/1574. 接雨水.cpp
@@ -22,3 +22,69 @@ int main(){
     cout << ans << endl;
     return 0;
 }
+
+// ###################################################### 版本2 ###################################################### //
+// 单调栈1
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 10;
+int n, h[N];
+stack<int> stk;
+
+int cal(){
+    int ret = 0;
+    for(int i = 1; i <= n; i++){
+        while(stk.size() && h[i] >= h[stk.top()]){
+            int t = stk.top();
+            stk.pop();
+            if(stk.empty()) break;
+            int left = stk.top();
+            int hh = min(h[i], h[left]) - h[t];
+            ret += hh * (i - left - 1);
+        }
+        stk.push(i);
+    }
+    return ret;
+}
+
+int main(){
+    cin >> n;
+    for(int i = 1; i <= n; i++) cin >> h[i];
+    cout << cal() << endl;
+    return 0;
+}
+
+// ###################################################### 版本3 ###################################################### //
+// 单调栈2
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 10;
+int n, h[N];
+stack<int> stk;
+
+int cal(){
+    int ret = 0;
+    for(int i = 1; i <= n; i++){
+        int last = 0;
+        while(stk.size() && h[i] >= h[stk.top()]){
+            int t = stk.top();
+            stk.pop();
+            ret += (h[t] - last) * (i - t - 1);
+            last = h[t];
+        }
+        if(stk.size()){
+            ret += (h[i] - last) * (i - stk.top() - 1);
+        }
+        stk.push(i);
+    }
+    return ret;
+}
+
+int main(){
+    cin >> n;
+    for(int i = 1; i <= n; i++) cin >> h[i];
+    cout << cal() << endl;
+    return 0;
+}
