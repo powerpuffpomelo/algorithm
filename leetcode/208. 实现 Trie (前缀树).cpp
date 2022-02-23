c549d02128d39b4b2dc14471fdb917d707496766
mello <powerpuffpomelo@gmail.com>
2022-02-23 17:18:40 +0800

    update

--- leetcode/208. 实现 Trie (前缀树).cpp
+++ leetcode/208. 实现 Trie (前缀树).cpp
@@ -1,49 +1,88 @@
+// ###################################################### 版本1 ###################################################### //
+// 结构体指针
 class Trie {
-public:
     struct node{
         bool is_end;
-        node* son[26];
+        vector<node*> children;
         node(){
             is_end = false;
-            for(int i = 0; i < 26; i++) son[i] = nullptr;
+            children = vector<node*>(26, nullptr);
         }
     }*root;
+public:
     Trie() {
-        root = new node();  // 需要写
+        root = new node();
     }
     
     void insert(string word) {
-        auto p = root;
+        node* p = root;
         for(char c : word){
-            if(!p->son[c - 'a']) p->son[c - 'a'] = new node();
-            p = p->son[c - 'a'];
+            int u = c - 'a';
+            if(!p->children[u]) p->children[u] = new node();
+            p = p->children[u];
         }
         p->is_end = true;
     }
     
     bool search(string word) {
-        auto p = root;
+        node* p = root;
         for(char c : word){
-            if(!p->son[c - 'a']) return false;
-            p = p->son[c - 'a'];
+            int u = c - 'a';
+            if(!p->children[u]) return false;
+            p = p->children[u];
         }
         return p->is_end;
     }
     
     bool startsWith(string prefix) {
-        auto p = root;
+        node* p = root;
         for(char c : prefix){
-            if(!p->son[c - 'a']) return false;
-            p = p->son[c - 'a'];
+            int u = c - 'a';
+            if(!p->children[u]) return false;
+            p = p->children[u];
         }
         return true;
     }
 };
 
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie* obj = new Trie();
- * obj->insert(word);
- * bool param_2 = obj->search(word);
- * bool param_3 = obj->startsWith(prefix);
- */
+// ###################################################### 版本2 ###################################################### //
+// 类本身作为trie树中节点
+class Trie {
+    bool is_end;
+    vector<Trie*> children;
+public:
+    Trie() {
+        is_end = false;
+        children = vector<Trie*>(26);
+    }
+    
+    void insert(string word) {
+        Trie* p = this;
+        for(char c : word){
+            int u = c - 'a';
+            if(!p->children[u]) p->children[u] = new Trie();
+            p = p->children[u];
+        }
+        p->is_end = true;
+    }
+    
+    bool search(string word) {
+        Trie* p = this;
+        for(char c : word){
+            int u = c - 'a';
+            if(!p->children[u]) return false;
+            p = p->children[u];
+        }
+        return p->is_end;
+    }
+    
+    bool startsWith(string prefix) {
+        Trie* p = this;
+        for(char c : prefix){
+            int u = c - 'a';
+            if(!p->children[u]) return false;
+            p = p->children[u];
+        }
+        return true;
+    }
+};
