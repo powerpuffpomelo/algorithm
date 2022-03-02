d9e03f187adc6524c5eb3e085b0f2fd9a1cc07f7
mello <powerpuffpomelo@gmail.com>
2022-03-02 14:41:01 +0800

    update

--- acwing/算法笔试面试辅导课/第13讲/1103. 棋盘游戏.cpp
+++ acwing/算法笔试面试辅导课/第13讲/1103. 棋盘游戏.cpp
@@ -50,3 +50,64 @@ int main(){
     cout << bfs(st, ed) << endl;
     return 0;
 }
+
+// ###################################################### 版本2 ###################################################### //
+// 二进制
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1 << 16;
+int st, ed;
+int dist[N];
+int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
+
+int input(){
+    int ret = 0;
+    for(int i = 0; i < 16; i++){
+        char c;
+        cin >> c;
+        if(c == '1') ret += 1 << i;
+    }
+    return ret;
+}
+
+int bit_swap(int t, int x, int y){
+    int ret = t;
+    int a = t >> x & 1, b = t >> y & 1;
+    ret -= a << x, ret -= b << y;
+    ret += a << y, ret += b << x;
+    return ret;
+}
+
+int bfs(int st, int ed){
+    if(st == ed) return 0;
+    queue<int> q;
+    q.push(st);
+    memset(dist, -1, sizeof dist);
+    dist[st] = 0;
+    while(q.size()){
+        int t = q.front();
+        q.pop();
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                for(int d = 0; d < 4; d++){
+                    int a = i + dx[d], b = j + dy[d];
+                    if(a < 0 || a >= 4 || b < 0 || b >= 4) continue;
+                    int tmp = bit_swap(t, i * 4 + j, a * 4 + b);
+                    if(dist[tmp] != -1) continue;
+                    if(tmp == ed) return dist[t] + 1;
+                    q.push(tmp);
+                    dist[tmp] = dist[t] + 1;
+                }
+            }
+        }
+    }
+    return -1;
+}
+
+int main(){
+    st = input();
+    ed = input();
+    cout << bfs(st, ed) << endl;
+    return 0;
+}
---
+++ acwing/算法笔试面试辅导课/第13讲/1111. 字母.cpp
@@ -0,0 +1,113 @@
+// ###################################################### 版本3 ###################################################### //
+// 更轻巧的dfs
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 26;
+char g[N][N];
+bool st[N];
+int r, s;
+int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
+
+int dfs(int x, int y){
+    int t = g[x][y] - 'A';
+    int sum = 0;
+    st[t] = 1;
+    for(int d = 0; d < 4; d++){
+        int a = x + dx[d], b = y + dy[d];
+        if(a < 0 || a >= r || b < 0 || b >= s) continue;
+        int k = g[a][b] - 'A';
+        if(st[k]) continue;
+        sum = max(sum, dfs(a, b));
+    }
+    st[t] = 0;
+    return sum + 1;
+}
+
+int main(){
+    cin >> r >> s;
+    for(int i = 0; i < r; i++) cin >> g[i];
+    cout << dfs(0, 0) << endl;
+    return 0;
+}
+
+// ###################################################### 版本2 ###################################################### //
+// dfs
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 26;
+char g[N][N];
+int r, s;
+int ans = 1;
+int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
+
+void dfs(int i, int j, unordered_set<char> se){
+    for(int d = 0; d < 4; d++){
+        int a = i + dx[d], b = j + dy[d];
+        if(a < 0 || a >= r || b < 0 || b >= s) continue;
+        if(se.count(g[a][b])) continue;
+        se.insert(g[a][b]);
+        dfs(a, b, se);
+        ans = max(ans, (int)se.size());
+        se.erase(g[a][b]);
+    }
+}
+
+int main(){
+    cin >> r >> s;
+    for(int i = 0; i < r; i++) cin >> g[i];
+    unordered_set<char> se;
+    se.insert(g[0][0]);
+    dfs(0, 0, se);
+    cout << ans << endl;
+    return 0;
+}
+
+// ###################################################### 版本1 ###################################################### //
+// 我的初版, bfs
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 26;
+char g[N][N];
+int r, s;
+int ans = 1;
+int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
+struct node{
+    int x, y, num;
+    bool vis[N];
+    node(int _x, int _y, int _num): x(_x), y(_y), num(_num) {
+        fill(vis, vis + N, false);
+    }
+};
+
+int bfs(node* st){
+    queue<node*> q;
+    q.push(st);
+    while(q.size()){
+        node* t = q.front();
+        q.pop();
+        int x = t->x, y = t->y;
+        for(int d = 0; d < 4; d++){
+            int a = x + dx[d], b = y + dy[d];
+            if(a < 0 || a >= r || b < 0 || b >= s || t->vis[g[a][b] - 'A']) continue;
+            node* tmp = new node(a, b, t->num + 1);
+            memcpy(tmp->vis, t->vis, sizeof(tmp->vis));
+            tmp->vis[g[a][b] - 'A'] = 1;
+            ans = max(ans, tmp->num);
+            q.push(tmp);
+        }
+    }
+    return ans;
+}
+
+int main(){
+    cin >> r >> s;
+    for(int i = 0; i < r; i++) cin >> g[i];
+    node* st = new node(0, 0, 1);
+    st->vis[g[0][0] - 'A'] = true;
+    cout << bfs(st) << endl;
+    return 0;
+}
+
