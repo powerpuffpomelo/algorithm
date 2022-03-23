4e46d7943133e80203ca5f33b36396f0a21b1d39
mello <powerpuffpomelo@gmail.com>
2022-03-24 00:01:40 +0800

    update

---
+++ leetcode/45. 跳跃游戏 II.cpp
@@ -0,0 +1,29 @@
+// ###################################################### 版本2 ###################################################### //
+// 绝绝子的dp，时间复杂度O(n)
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        vector<int> dp(nums.size());
+        for(int i = 1, j = 0; i < nums.size(); i++){
+            while(j + nums[j] < i) j++;
+            dp[i] = dp[j] + 1;
+        }
+        return dp[nums.size() - 1];
+    }
+};
+
+// ###################################################### 版本1 ###################################################### //
+// 我的初版 dp，时间复杂度O(n^2)
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        vector<int> dp(nums.size(), 1e4 + 10);
+        dp[0] = 0;
+        for(int i = 1; i < nums.size(); i++){
+            for(int j = 0; j < i; j++){
+                if(nums[j] >= i - j) dp[i] = min(dp[i], dp[j] + 1);
+            }
+        }
+        return dp[nums.size() - 1];
+    }
+};
