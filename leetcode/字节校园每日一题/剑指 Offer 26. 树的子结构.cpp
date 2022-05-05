59cd45d8241ecdebb06aad8601bed08cb3c8150b
mello <powerpuffpomelo@gmail.com>
2022-05-05 14:32:21 +0800

    update

--- leetcode/字节校园每日一题/剑指 Offer 26. 树的子结构.cpp
+++ leetcode/字节校园每日一题/剑指 Offer 26. 树的子结构.cpp
@@ -1,5 +1,21 @@
+// ###################################################### 版本2 ###################################################### //
+// 原函数可以直接递归 O(nm) n为A节点数，m为B节点数
+class Solution {
+public:
+    bool isSubStructure(TreeNode* A, TreeNode* B) {
+        if(!A || !B) return false;
+        if(isSame(A, B)) return true;
+        return isSubStructure(A->left, B) || isSubStructure(A->right, B);
+    }
+    bool isSame(TreeNode* A, TreeNode* B){
+        if(!B) return true;
+        if(!A || A->val != B->val) return false;
+        return isSame(A->left, B->left) && isSame(A->right, B->right);
+    }
+};
+
 // ###################################################### 版本1 ###################################################### //
-// 我的初版，比较奇特
+// 我的初版
 class Solution {
 public:
     bool same(TreeNode* a, TreeNode* b){  // b刚好是a从根节点开始的子树
