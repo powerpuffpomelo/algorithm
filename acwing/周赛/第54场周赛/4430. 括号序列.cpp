3e9e859d60186f980d0a7967dbc7b6d1ede365ec
mello <powerpuffpomelo@gmail.com>
2022-06-09 00:14:56 +0800

    update

--- acwing/周赛/第54场周赛/4430. 括号序列.cpp
+++ acwing/周赛/第54场周赛/4430. 括号序列.cpp
@@ -1,3 +1,54 @@
+// ###################################################### 版本2 ###################################################### //
+// 很优雅的方法！如果遇到cnt<0的情况，则此前的右括号数量就是可选项。然后检查一下后边是否合法就可
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e6 + 10;
+int n;
+char s[N];
+
+int work(){
+    int cnt = 0, r = 0;
+    for(int i = 0; i < n; i++){
+        if(s[i] == '(') cnt++;
+        else{
+            cnt--;
+            r++;
+            if(cnt < 0){
+                cnt += 2;
+                for(int j = i + 1; j < n; j++){
+                    if(s[j] == '(') cnt++;
+                    else{
+                        cnt--;
+                        if(cnt < 0) return 0;
+                    }
+                }
+                return r;
+            }
+        }
+    }
+    return 0;
+}
+
+int main(){
+    scanf("%d%s", &n, s);
+    int l = 0, r = 0;
+    for(int i = 0; i < n; i++){
+        if(s[i] == '(') l++;
+        else r++;
+    }
+    if(r - l == 2) cout << work() << endl;
+    else if(l - r == 2){
+        reverse(s, s + n);
+        for(int i = 0; i < n; i++){
+            if(s[i] == '(') s[i] = ')';
+            else s[i] = '(';
+        }
+        cout << work() << endl;
+    }else cout << 0 << endl;
+    return 0;
+}
+
 // ###################################################### 版本1 ###################################################### //
 // 我的初版
 #include <bits/stdc++.h>
