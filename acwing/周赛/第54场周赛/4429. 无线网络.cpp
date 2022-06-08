f17ec0e883190dd919fa88bfff8b40f18b842f91
mello <powerpuffpomelo@gmail.com>
2022-06-08 23:36:55 +0800

    update

--- acwing/周赛/第54场周赛/4429. 无线网络.cpp
+++ acwing/周赛/第54场周赛/4429. 无线网络.cpp
@@ -32,3 +32,41 @@ int main(){
     cout << ans << endl;
     return 0;
 }
+
+// ###################################################### 版本2 ###################################################### //
+// 后算dist
+#include <bits/stdc++.h>
+using namespace std;
+
+#define x first
+#define y second
+typedef long long LL;
+typedef pair<int, int> pii;
+const int N = 2010;
+int n;
+pii s1, s2;
+pii q[N];
+
+LL dist(pii a, pii b){
+    LL dx = a.x - b.x;
+    LL dy = a.y - b.y;
+    return dx * dx + dy * dy;
+}
+
+bool cmp(pii a, pii b){
+    return dist(a, s1) < dist(b, s1);
+}
+
+int main(){
+    cin >> n >> s1.x >> s1.y >> s2.x >> s2.y;
+    for(int i = 0; i < n; i++) cin >> q[i].x >> q[i].y;
+    sort(q, q + n, cmp);
+    LL ans = 1e18, r = 0;
+    for(int i = n - 1; i >= 0; i--){
+        ans = min(ans, dist(q[i], s1) + r);
+        r = max(r, dist(q[i], s2));
+    }
+    ans = min(ans, r);
+    cout << ans << endl;
+    return 0;
+}
