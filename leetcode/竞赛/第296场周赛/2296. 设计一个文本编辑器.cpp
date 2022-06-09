34b853fd89ffb3df3b82b6e7a07b8a07ca01b601
mello <powerpuffpomelo@gmail.com>
2022-06-09 20:29:48 +0800

    update

--- leetcode/竞赛/第296场周赛/2296. 设计一个文本编辑器.cpp
+++ leetcode/竞赛/第296场周赛/2296. 设计一个文本编辑器.cpp
@@ -1,5 +1,5 @@
 // ###################################################### 版本1 ###################################################### //
-// 
+// string对顶栈
 class TextEditor {
 public:
     string s, t;
@@ -35,5 +35,74 @@ public:
         }
         string ans(s.end() - min(10, int(s.size())), s.end());
         return ans;
+    }
+};
+
+
+// ###################################################### 版本2 ###################################################### //
+// 自定义双向链表
+struct node{
+    char c;
+    node *prev, *next;
+    node(char _c): c(_c) {}
+};
+
+class TextEditor {
+private:
+    node *st, *ed, *cur;
+
+    string get_left_string(int k){
+        string ret;
+        auto p = cur;
+        for(; k > 0 && p != st; k--){
+            ret += p->c;
+            p = p->prev;
+        }
+        reverse(ret.begin(), ret.end());
+        return ret;
+    }
+
+public:
+    TextEditor() {
+        st = new node('!');
+        ed = new node('!');
+        st->next = ed;
+        ed->prev = st;
+        cur = st;
+    }
+    
+    void addText(string text) {
+        for(char c : text){
+            auto t = new node(c);
+            t->next = cur->next;
+            cur->next->prev = t;
+            t->prev = cur;
+            cur = cur->next = t;
+        }
+    }
+    
+    int deleteText(int k) {
+        int num = 0;
+        for(; k > 0 && cur != st; k--){
+            cur->prev->next = cur->next;
+            cur->next->prev = cur->prev;
+            cur = cur->prev;
+            num++;
+        }
+        return num;
+    }
+    
+    string cursorLeft(int k) {
+        for(; k > 0 && cur != st; k--){
+            cur = cur->prev;
+        }
+        return get_left_string(10);
+    }
+    
+    string cursorRight(int k) {
+        for(; k > 0 && cur->next != ed; k--){
+            cur = cur->next;
+        }
+        return get_left_string(10);
     }
 };
