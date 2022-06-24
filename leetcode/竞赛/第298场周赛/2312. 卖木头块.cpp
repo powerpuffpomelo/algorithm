41da09e67f7c8b6d26cf638f5edd7201deb15b0a
mello <powerpuffpomelo@gmail.com>
2022-06-24 15:23:47 +0800

    update

---
+++ leetcode/竞赛/第298场周赛/2312. 卖木头块.cpp
@@ -0,0 +1,23 @@
+// ###################################################### 版本1 ###################################################### //
+// dp
+class Solution {
+public:
+    long long sellingWood(int m, int n, vector<vector<int>>& prices) {
+        vector<vector<long long>> dp(m + 1, vector<long long>(n + 1));
+        for(auto p : prices){
+            int h = p[0], w = p[1], v = p[2];
+            dp[h][w] = v;
+        }
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                for(int k = 1; k < i; k++){
+                    dp[i][j] = max(dp[i][j], dp[k][j] + dp[i - k][j]);
+                }
+                for(int l = 1; l < j; l++){
+                    dp[i][j] = max(dp[i][j], dp[i][l] + dp[i][j - l]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
