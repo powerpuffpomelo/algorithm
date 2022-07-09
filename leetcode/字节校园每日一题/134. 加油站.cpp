c43ffaaa3392fda1d3b441eef9823e1b7a2b6379
mello <powerpuffpomelo@gmail.com>
2022-07-09 11:40:53 +0800

    update

--- leetcode/字节校园每日一题/134. 加油站.cpp
+++ leetcode/字节校园每日一题/134. 加油站.cpp
@@ -5,7 +5,7 @@ class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int n = gas.size();
-        for(int i = 0, j; i < n; ){
+        for(int i = 0, j; i < n; ){  // 枚举起点，如果从i开始不能走到k，那么从i和k中间的任意位置开始都不可能是答案
             int left = 0;
             for(j = 0; j < n; j++){
                 int k = (i + j) % n;
