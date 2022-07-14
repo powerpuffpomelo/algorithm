db9678bdddff334b11e25c4bd8e36fade533be5c
mello <powerpuffpomelo@gmail.com>
2022-07-14 21:11:40 +0800

    update

--- leetcode/竞赛/第301场周赛/2338. 统计理想数组的数目.cpp
+++ leetcode/竞赛/第301场周赛/2338. 统计理想数组的数目.cpp
@@ -1,3 +1,31 @@
+// ###################################################### 版本2 ###################################################### //
+// dp
+class Solution {
+public:
+    int idealArrays(int n, int m) {
+        const int MOD = 1e9 + 7;
+        vector<vector<int>> f(m + 1, vector<int>(15));
+        for (int i = 1; i <= m; i ++ ) f[i][1] = 1;
+
+        for (int j = 1; j < 14; j ++ )
+            for (int i = 1; i <= m; i ++ )
+                for (int k = 2; k * i <= m; k ++ )
+                    f[k * i][j + 1] = (f[k * i][j + 1] + f[i][j]) % MOD;
+
+        vector<vector<int>> C(n, vector<int>(15));
+        for (int i = 0; i < n; i ++ )
+            for (int j = 0; j <= i && j <= 14; j ++ )
+                if (!j) C[i][j] = 1;
+                else C[i][j] = (C[i - 1][j] + C[i - 1][j - 1]) % MOD;
+
+        int res = 0;
+        for (int i = 1; i <= m; i ++ )
+            for (int j = 1; j <= 14 && j <= n; j ++ )
+                res = (res + (long long)f[i][j] * C[n - 1][j - 1]) % MOD;
+        return res;
+    }
+};
+
 // ###################################################### 版本1 ###################################################### //
 // dfs
 class Solution {
