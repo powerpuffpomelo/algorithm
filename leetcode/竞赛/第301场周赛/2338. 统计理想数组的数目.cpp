d38ee6f0a049a8e69402f09bf1b6614ba7e51143
mello <powerpuffpomelo@gmail.com>
2022-07-15 13:47:59 +0800

    update

--- leetcode/竞赛/第301场周赛/2338. 统计理想数组的数目.cpp
+++ leetcode/竞赛/第301场周赛/2338. 统计理想数组的数目.cpp
@@ -4,15 +4,15 @@ class Solution {
 public:
     int idealArrays(int n, int m) {
         const int MOD = 1e9 + 7;
-        vector<vector<int>> f(m + 1, vector<int>(15));
-        for (int i = 1; i <= m; i ++ ) f[i][1] = 1;
+        vector<vector<int>> dp(m + 1, vector<int>(15));   // dp[i][j]: 以i为结尾，数组中不同数字的长度为j，的方案数
+        for (int i = 1; i <= m; i ++ ) dp[i][1] = 1;
 
         for (int j = 1; j < 14; j ++ )
             for (int i = 1; i <= m; i ++ )
                 for (int k = 2; k * i <= m; k ++ )
-                    f[k * i][j + 1] = (f[k * i][j + 1] + f[i][j]) % MOD;
+                    dp[k * i][j + 1] = (dp[k * i][j + 1] + dp[i][j]) % MOD;
 
-        vector<vector<int>> C(n, vector<int>(15));
+        vector<vector<int>> C(n, vector<int>(15));  // 组合数
         for (int i = 0; i < n; i ++ )
             for (int j = 0; j <= i && j <= 14; j ++ )
                 if (!j) C[i][j] = 1;
@@ -21,7 +21,7 @@ public:
         int res = 0;
         for (int i = 1; i <= m; i ++ )
             for (int j = 1; j <= 14 && j <= n; j ++ )
-                res = (res + (long long)f[i][j] * C[n - 1][j - 1]) % MOD;
+                res = (res + (long long)dp[i][j] * C[n - 1][j - 1]) % MOD;
         return res;
     }
 };
@@ -33,10 +33,10 @@ public:
     int n, m;
     int ans = 0;
     const int MOD = 1e9 + 7;
-    vector<vector<int>> f;
+    vector<vector<int>> C;
     
     void dfs(int u, int cnt) {
-        ans = (ans + f[n - 1][cnt - 1]) % MOD;
+        ans = (ans + C[n - 1][cnt - 1]) % MOD;
         if (cnt < n) {
             for (int i = 2; i * u <= m; i ++ )
                 dfs(i * u, cnt + 1);
@@ -45,11 +45,11 @@ public:
     
     int idealArrays(int n, int m) {
         this->n = n, this->m = m;
-        f = vector<vector<int>>(n, vector<int>(20));
+        C = vector<vector<int>>(n, vector<int>(20));
         for (int i = 0; i < n; i ++ )
             for (int j = 0; j < 20 && j <= i; j ++ )
-                if (!j) f[i][j] = 1;
-                else f[i][j] = (f[i - 1][j] + f[i - 1][j - 1]) % MOD;
+                if (!j) C[i][j] = 1;
+                else C[i][j] = (C[i - 1][j] + C[i - 1][j - 1]) % MOD;
         
         for (int i = 1; i <= m; i ++ )
             dfs(i, 1);
