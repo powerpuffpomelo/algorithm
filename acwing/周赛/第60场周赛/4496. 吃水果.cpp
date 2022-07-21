f7fc1c8039c169f17ef4486f6eb68c34b564db65
mello <powerpuffpomelo@gmail.com>
2022-07-21 13:33:47 +0800

    update

--- acwing/周赛/第60场周赛/4496. 吃水果.cpp
+++ acwing/周赛/第60场周赛/4496. 吃水果.cpp
@@ -1,19 +1,19 @@
 // ###################################################### 版本1 ###################################################### //
-// 我的初版
+// dp
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 2010;
-const long long mod = 998244353;
+const int N = 2010, mod = 998244353;
 int n, m, k;
-long long dp[N][N];   // 发了i个小朋友，其中j个小朋友拿到和左边不同
+int dp[N][N];   // 发了i个小朋友，其中j个小朋友拿到和左边不同
 
 int main(){
     cin >> n >> m >> k;
     dp[1][0] = m;
     for(int i = 2; i <= n; i++){
         for(int j = 0; j <= k && j < i; j++){
-            dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - 1] * (m - 1)) % mod;
+            dp[i][j] = dp[i - 1][j];
+            if(j) dp[i][j] = (dp[i][j] + dp[i - 1][j - 1] * (m - 1ll)) % mod;   // 这里1ll，dp数组就无需long long了
         }
     }
     cout << dp[n][k] << endl;
