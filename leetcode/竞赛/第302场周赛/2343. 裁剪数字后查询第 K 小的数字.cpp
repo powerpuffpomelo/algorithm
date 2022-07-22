87103dff53019d6703f12c4032a456b19b2400cf
mello <powerpuffpomelo@gmail.com>
2022-07-22 18:34:40 +0800

    update

--- leetcode/竞赛/第302场周赛/2343. 裁剪数字后查询第 K 小的数字.cpp
+++ leetcode/竞赛/第302场周赛/2343. 裁剪数字后查询第 K 小的数字.cpp
@@ -2,29 +2,21 @@
 // 我的初版
 class Solution {
 public:
-    typedef pair<string, int> pii;
-    static bool cmp(pii& a, pii& b){
-        if(a.first != b.first) return a.first < b.first;
-        return a.second < b.second;
-    }
+    typedef pair<string, int> psi;
     vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
         int n = nums.size(), len = nums[0].size();
-        vector<vector<pii>> vv(len + 1);
+        vector<vector<psi>> vv;
         for(int l = 1; l <= len; l++){
-            //cout << l << endl;
-            vector<pii> temp;
+            vector<psi> temp;
             for(int i = 0; i < n; i++){
                 temp.push_back({nums[i].substr(len - l), i});
-                //if(l == 2) cout << nums[i] << ' ' << temp[i].first << endl;
             }
-            //break;
-            sort(temp.begin(), temp.end(), cmp);
-            vv[l] = temp;
+            sort(temp.begin(), temp.end());
+            vv.push_back(temp);
         }
-        //return 0;
         vector<int> ans;
         for(auto q : queries){
-            auto temp = vv[q[1]];
+            vector<psi> temp = vv[q[1] - 1];
             ans.push_back(temp[q[0] - 1].second);
         }
         return ans;
