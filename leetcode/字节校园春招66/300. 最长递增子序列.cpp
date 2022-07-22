68f33c18507699e127799ddb6ac8bda70c0e6989
mello <powerpuffpomelo@gmail.com>
2022-07-22 17:50:59 +0800

    update

--- leetcode/字节校园春招66/300. 最长递增子序列.cpp
+++ leetcode/字节校园春招66/300. 最长递增子序列.cpp
@@ -1,4 +1,22 @@
-// dp[i] 截止到第i位的最长递增子序列长度，其取值只和dp[0]到dp[i - 1]有关，取值一旦固定了是不会变的
+// ###################################################### 版本2 ###################################################### //
+// 时间复杂度O(nlogn)
+class Solution {
+public:
+    static const int N = 2510;
+    int len2num[N], maxlen = 0;
+    int lengthOfLIS(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++){
+            int len = maxlen;
+            while(len > 0 && nums[i] <= len2num[len]) len--;
+            len2num[len + 1] = nums[i];
+            maxlen = max(maxlen, len + 1);
+        }
+        return maxlen;
+    }
+};
+
+// ###################################################### 版本1 ###################################################### //
+// 时间复杂度O(n^2)
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
