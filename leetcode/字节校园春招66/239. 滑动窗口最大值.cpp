9329376f5ee8c4a8424cb73178c7f98a6bce9061
mello <powerpuffpomelo@gmail.com>
2022-08-15 18:19:51 +0800

    update

--- leetcode/字节校园春招66/239. 滑动窗口最大值.cpp
+++ leetcode/字节校园春招66/239. 滑动窗口最大值.cpp
@@ -21,13 +21,13 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n = nums.size();
-        int q[n], front = -1, rear = -1;
+        int q[n], front = 0, rear = -1;
         vector<int> ans;
         for(int i = 0; i < n; i++){
-            while(rear > front && nums[q[rear]] <= nums[i]) rear--;
+            while(rear >= front && nums[q[rear]] <= nums[i]) rear--;
             q[++rear] = i;
-            while(q[front + 1] < i - k + 1) front++;
-            if(i >= k - 1) ans.push_back(nums[q[front + 1]]);
+            while(q[front] < i - k + 1) front++;
+            if(i >= k - 1) ans.push_back(nums[q[front]]);
         }
         return ans;
     }
