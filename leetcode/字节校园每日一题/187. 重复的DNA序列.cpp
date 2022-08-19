874ee8f19eddff30fb83275791b1cf3a8a6dd696
mello <powerpuffpomelo@gmail.com>
2022-08-19 15:02:54 +0800

    update

--- leetcode/字节校园每日一题/187. 重复的DNA序列.cpp
+++ leetcode/字节校园每日一题/187. 重复的DNA序列.cpp
@@ -1,3 +1,37 @@
+// ###################################################### 版本2 ###################################################### //
+// 优化一点点写法
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        vector<string> ans;
+        unordered_map<string, int> ma;
+        for(int i = 0; i + 10 - 1 < s.size(); i++){
+            string str = s.substr(i, 10);
+            if(ma[str] == 1) ans.push_back(str);
+            ma[str] += 1;
+        }
+        return ans;
+    }
+};
+
+// ###################################################### 版本3 ###################################################### //
+// map遍历
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        unordered_map<string, int> ma;
+        for(int i = 0; i + 10 - 1 < s.size(); i++){
+            string str = s.substr(i, 10);
+            ma[str]++;
+        }
+        vector<string> ans;
+        for(auto [k, v] : ma){
+            if(v > 1) ans.push_back(k);
+        }
+        return ans;
+    }
+};
+
 // ###################################################### 版本1 ###################################################### //
 // 我的初版
 class Solution {
