4a51ecf02bc5579266985a0017ec6a35157842c5
mello <powerpuffpomelo@gmail.com>
2022-08-28 13:23:21 +0800

    update

--- leetcode/字节校园每日一题/435. 无重叠区间.cpp
+++ leetcode/字节校园每日一题/435. 无重叠区间.cpp
@@ -1,3 +1,24 @@
+// ###################################################### 版本2 ###################################################### //
+// 优化一点点~
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b){  // 匿名函数
+            return a[1] < b[1];   // 仅考虑右端点即可
+        });
+        if(intervals.empty()) return 0;
+        int ans = 1, r = intervals[0][1];
+        for(int i = 1; i < n; i++){
+            if(intervals[i][0] >= r){
+                ans++;
+                r = intervals[i][1];
+            }
+        }
+        return n - ans;
+    }
+};
+
 // ###################################################### 版本1 ###################################################### //
 // 我的初版
 class Solution {
