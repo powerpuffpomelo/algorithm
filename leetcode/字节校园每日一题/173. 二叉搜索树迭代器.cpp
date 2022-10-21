5fd53da81cee62f82b1201229e0c36f752e4860b
mello <powerpuffpomelo@gmail.com>
2022-10-21 16:41:30 +0800

    update

---
+++ leetcode/字节校园每日一题/173. 二叉搜索树迭代器.cpp
@@ -0,0 +1,36 @@
+// ###################################################### 版本1 ###################################################### //
+// 我的初版
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+public:
+    vector<int> vv;
+    int p;
+
+    void dfs(TreeNode* root){
+        if(root->left) dfs(root->left);
+        vv.push_back(root->val);
+        if(root->right) dfs(root->right);
+    }
+    BSTIterator(TreeNode* root) {
+        dfs(root);
+        p = 0;
+    }
+    
+    int next() {
+        return vv[p++];
+    }
+    
+    bool hasNext() {
+        return p < vv.size();
+    }
+};
