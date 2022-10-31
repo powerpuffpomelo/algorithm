56a02347d991cf7cb1fed27c81ad69df0962cbaa
mello <powerpuffpomelo@gmail.com>
2022-10-31 16:40:31 +0800

    update

--- leetcode/字节校园每日一题/410. 分割数组的最大值.cpp
+++ leetcode/字节校园每日一题/410. 分割数组的最大值.cpp
@@ -1,10 +1,10 @@
 // ###################################################### 版本1 ###################################################### //
-// 我的初版
+// 我的初版, dp
 class Solution {
 public:
     int splitArray(vector<int>& nums, int m) {
         int n = nums.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, INT_MAX));
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, INT_MAX));  // dp[i][k] 前i个数被分为k组，每组最大值的最小值
         vector<int> s(n + 1);
         for(int i = 1; i <= n; i++) s[i] = s[i - 1] + nums[i - 1];
         dp[0][0] = 0;
