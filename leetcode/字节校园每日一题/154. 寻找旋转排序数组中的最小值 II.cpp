752eafe8a200f0a7291f15bf9fc0a5d48015082a
mello <powerpuffpomelo@gmail.com>
2022-11-21 23:50:34 +0800

    update

--- leetcode/字节校园每日一题/154. 寻找旋转排序数组中的最小值 II.cpp
+++ leetcode/字节校园每日一题/154. 寻找旋转排序数组中的最小值 II.cpp
@@ -5,7 +5,7 @@ public:
         while(l < r && nums[r] == nums[l]) r--;
         while(l < r){
             int mid = l + r >> 1;
-            if(nums[mid] <= nums[r]) r = mid;
+            if(nums[mid] <= nums[r]) r = mid;  // 注意这里nums[r]不能改成nums[l]，因为nums[l]本身可能是最小值
             else l = mid + 1;
         }
         return nums[l];
