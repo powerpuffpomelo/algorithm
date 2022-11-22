4a7970ddecf7574ec6db33bad4d098adb65c8a87
mello <powerpuffpomelo@gmail.com>
2022-11-22 19:25:19 +0800

    update

--- leetcode/字节校园每日一题/147. 对链表进行插入排序.cpp
+++ leetcode/字节校园每日一题/147. 对链表进行插入排序.cpp
@@ -1,5 +1,46 @@
+// ###################################################### 版本3 ###################################################### //
+// 链表插入排序
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        auto dummy = new ListNode(-1);  // 新建一个链表
+        for(auto p = head; p; ){  // 每次从原链表中取出一个元素，插入新链表
+            auto cur = dummy, next = p->next;
+            while(cur->next && cur->next->val <= p->val) cur = cur->next;  // 新链表从前往后寻找插入位置
+            p->next = cur->next;
+            cur->next = p;
+            p = next;
+        }
+        return dummy->next;
+    }
+};
+
+// ###################################################### 版本2 ###################################################### //
+// 借助vector插入排序
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        vector<ListNode*> vec;
+        for(auto p = head; p; p = p->next){
+            vec.push_back(p);
+        }
+        for(int i = 1; i < vec.size(); i++){
+            int j = i;
+            while(j > 0 && vec[j]->val < vec[j - 1]->val){
+                swap(vec[j], vec[j - 1]);
+                j--;
+            }
+        }
+        for(int i = 0; i < vec.size(); i++){
+            if(i < vec.size() - 1) vec[i]->next = vec[i + 1];
+            else vec[i]->next = nullptr;
+        }
+        return vec[0];
+    }
+};
+
 // ###################################################### 版本1 ###################################################### //
-// 我的初版
+// 偷懒，非插入排序
 /**
  * Definition for singly-linked list.
  * struct ListNode {
