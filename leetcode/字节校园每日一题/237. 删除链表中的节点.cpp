15c968d108b4622cd313ef5a2319b3ffffd5f6ce
mello <powerpuffpomelo@gmail.com>
2022-11-26 19:53:11 +0800

    update

---
+++ leetcode/字节校园每日一题/237. 删除链表中的节点.cpp
@@ -0,0 +1,17 @@
+// ###################################################### 版本1 ###################################################### //
+// 复制下一个节点值，转化成删除下一个节点
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
