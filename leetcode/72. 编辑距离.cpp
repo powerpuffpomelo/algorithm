a8dc9e0b2ddfd234f8dcbc79c01c200ba4ca0c77
mello <powerpuffpomelo@gmail.com>
2022-12-14 16:45:44 +0800

    update

--- leetcode/72. 编辑距离.cpp
+++ leetcode/72. 编辑距离.cpp
@@ -1,19 +1,20 @@
+// ###################################################### 版本1 ###################################################### //
+// 
 class Solution {
 public:
-    vector<vector<int>> dp;
     int minDistance(string word1, string word2) {
-        word1 = ' ' + word1, word2 = ' ' + word2;
         int n = word1.size(), m = word2.size();
-        dp = vector<vector<int>>(n, vector<int>(m));
-        for(int i = 1; i < n; i++) dp[i][0] = i;   // 记得初始化！
-        for(int j = 1; j < m; j++) dp[0][j] = j;
-        for(int i = 1; i < n; i++){
-            for(int j = 1; j < m; j++){
-                int ad = 0;
-                if(word1[i] != word2[j]) ad = 1;
-                dp[i][j] = min(dp[i - 1][j - 1] + ad, min(dp[i][j - 1], dp[i - 1][j]) + 1);
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 1e9));
+        word1 = ' ' + word1;
+        word2 = ' ' + word2;
+        for(int i = 0; i <= n; i++) dp[i][0] = i;   // 记得初始化！
+        for(int j = 0; j <= m; j++) dp[0][j] = j;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + 1;
+                dp[i][j] = min(dp[i][j], dp[i - 1][j - 1] + (word1[i] == word2[j] ? 0 : 1));
             }
         }
-        return dp[n - 1][m - 1];
+        return dp[n][m];
     }
 };
