bfe864716f1095047bd8fe529b797c4efaeddf49
mello <powerpuffpomelo@gmail.com>
2023-02-24 11:02:08 +0800

    update

--- leetcode/62. 不同路径.cpp
+++ leetcode/62. 不同路径.cpp
@@ -14,3 +14,16 @@ public:
         return dp[m - 1][n - 1];
     }
 };
+
+// ###################################################### 版本2 ###################################################### //
+// 组合数学，从m+n-2次移动中选择m-1次
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        long long ans = 1;
+        for(int x = n, y = 1; y < m; x++, y++){
+            ans = ans * x / y;
+        }
+        return ans;
+    }
+};
