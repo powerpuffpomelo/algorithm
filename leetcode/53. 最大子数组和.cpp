d5e19ac4a683ecad5ab3126bd673d4d35c4cf327
mello <powerpuffpomelo@gmail.com>
2023-04-18 21:00:59 +0800

    update

--- leetcode/53. 最大子数组和.cpp
+++ leetcode/53. 最大子数组和.cpp
@@ -1,11 +1,15 @@
+/*
+本题是dp
+dp[i]表示以第i个数为结尾的区间的最大值
+dp[i] = max(dp[i], dp[i - 1] + nums[i]);
+*/
 class Solution {
 public:
-    int ans = -1e4 - 10;
     int maxSubArray(vector<int>& nums) {
-        int temp = 0;
-        for(int i = 0; i < nums.size(); i++){
-            temp = max(temp, 0) + nums[i];
-            ans = max(ans, temp);
+        int ans = INT_MIN;
+        for(int i = 0, last = 0; i < nums.size(); i++){
+            last = nums[i] + max(last, 0);   // dp[i]只需要利用dp[i - 1]，所以可以用last变量滚动记录
+            ans = max(ans, last);
         }
         return ans;
     }
