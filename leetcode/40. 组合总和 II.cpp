eb7c53fb3f1778f5e70a527c922bd93e8ded1fec
mello <powerpuffpomelo@gmail.com>
2023-08-27 23:49:03 +0800

    update

--- leetcode/40. 组合总和 II.cpp
+++ leetcode/40. 组合总和 II.cpp
@@ -5,7 +5,7 @@ public:
     vector<vector<int>> ans;
     int n;
     void dfs(vector<int>& candidates, int target, int u, vector<int>& vec){
-        if(target == 0){
+        if(target == 0){        // 注意顺序！target==0要放在判断u>=n之前，否则可能错过 选最后一个数的正确答案
             ans.push_back(vec);
             return;
         }
@@ -13,7 +13,7 @@ public:
         int x = candidates[u];
         // 不放当前数
         int k = u + 1;
-        while(k < n && candidates[k] == x) k++;  // 控制答案不能重复，要放的话就可着前边的放，不放的话就跳到下一个数。（比如有5个2，只需要2个2，就取前两个）
+        while(k < n && candidates[k] == x) k++;  // 控制答案不能重复，要放的话就可着前边的放，不放的话就跳到下一个数。（比如一共有五个2，但只需要两个2，就取前两个）
         dfs(candidates, target, k, vec);
         // 放当前数
         vec.push_back(x);
