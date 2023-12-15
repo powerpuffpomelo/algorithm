549e3ffe912dcff26326c472f0371fc5acb759f7
mello <powerpuffpomelo@gmail.com>
2023-12-15 10:51:26 +0800

    Update 868. 筛质数.cpp

--- acwing/算法基础课/第4讲 数学知识/质数/868. 筛质数.cpp
+++ acwing/算法基础课/第4讲 数学知识/质数/868. 筛质数.cpp
@@ -28,6 +28,12 @@ int main(){
 // i是质数就筛掉i与primes中所有质数的乘积；i是非质数筛掉i与primes中所有<=最小质因子的乘积
 // 关于break的点: i % primes[j] == 0，则 i = x * primes[j]，如果要筛 i * primes[j + 1] 的话，就相当于筛 x * primes[j] * primes[j + 1]；
 // 而这个数本应被更小的 primes[j] 筛掉
+
+// 任何一个合数x都会被筛掉：如果x的最小质因子是pj，当i枚举到x/pj的时候，这个合数就会被筛掉。
+// 任何一个>=2的自然数都有一个最小质因子，而且线性筛法只用最小质因子来筛。
+// 关于break能保证每个数只被最小质因子筛掉：
+// 若 i % pj == 0，则pj一定是i的最小质因子，pj一定是pj*i的最小质因子
+// 若 i % pj != 0，则pj一定小于i的所有质因子，pj一定是pj*i的最小质因子
 #include <iostream>
 using namespace std;
 
