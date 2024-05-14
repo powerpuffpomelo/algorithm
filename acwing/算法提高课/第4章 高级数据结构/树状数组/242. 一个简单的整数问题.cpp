296ffc9ca5a1cf9ab1d8b538a32c3097e41d961c
powerpuffpomelo <powerpuffpomelo@gmail.com>
2024-05-14 17:38:08 +0800

    update

--- acwing/算法提高课/第4章 高级数据结构/树状数组/242. 一个简单的整数问题.cpp
+++ acwing/算法提高课/第4章 高级数据结构/树状数组/242. 一个简单的整数问题.cpp
@@ -1,41 +1,42 @@
 // ###################################################### 版本1 ###################################################### //
-// 我的初版
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
 const int N = 1e5 + 10;
-int n, m;
-int a[N], tr[N];
+int n, m, l, r, d, x;
+ll a[N], tr[N];
 string op;
-int l, r, d, x;
 
 int lowbit(int x){
     return x & -x;
 }
 
-void add(int x, int c){
+void modify(int x, int c){
     for(int i = x; i <= n; i += lowbit(i)) tr[i] += c;
 }
 
-int sum(int x){
-    int ret = 0;
+ll query(int x){
+    ll ret = 0;
     for(int i = x; i; i -= lowbit(i)) ret += tr[i];
     return ret;
 }
 
 int main(){
     cin >> n >> m;
-    for(int i = 1; i <= n; i++) cin >> a[i];
-    for(int i = 1; i <= n; i++) add(i, a[i] - a[i - 1]);
+    for(int i = 1; i <= n; i++){
+        cin >> a[i];
+        modify(i, a[i] - a[i - 1]);
+    }
     while(m--){
         cin >> op;
         if(op == "C"){
             cin >> l >> r >> d;
-            add(l, d);
-            add(r + 1, -d);
-        }else{
+            modify(l, d);
+            if(r < n) modify(r + 1, -d);
+        }else if(op == "Q"){
             cin >> x;
-            cout << sum(x) << endl;
+            cout << query(x) << endl;
         }
     }
     return 0;
