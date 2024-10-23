c8edc415c5dfafb1e0e90c329a2835cda099a674
powerpuffpomelo <powerpuffpomelo@gmail.com>
2024-10-23 16:56:34 +0800

    update

--- acwing/算法提高课/第1章 动态规划/数字三角形模型/1027. 方格取数.cpp
+++ acwing/算法提高课/第1章 动态规划/数字三角形模型/1027. 方格取数.cpp
@@ -1,3 +1,27 @@
+// ###################################################### 版本2 ###################################################### //
+// 代码精简版
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 15;
+int n, a, b, c;
+int g[N][N], dp[N * 2][N][N];
+
+int main(){
+    cin >> n;
+    while(cin >> a >> b >> c, a || b || c) g[a][b] = c;
+    for(int k = 2; k <= n + n; k++){
+        for(int i1 = 1; i1 <= n && i1 <= k - 1; i1++){
+            for(int i2 = 1; i2 <= n && i2 <= k - 1; i2++){
+                dp[k][i1][i2] = max({dp[k - 1][i1 - 1][i2 - 1], dp[k - 1][i1 - 1][i2], dp[k - 1][i1][i2 - 1], dp[k - 1][i1][i2]});
+                dp[k][i1][i2] += (i1 == i2) ? g[i1][k - i1] : (g[i1][k - i1] + g[i2][k - i2]);
+            }
+        }
+    }
+    cout << dp[n * 2][n][n] << endl;
+    return 0;
+}
+
 // ###################################################### 版本1 ###################################################### //
 // dp，两个路线并行计算
 // 番外：dp是图论的子集，绝大多数dp都可以转换成最短路问题；当最短路问题是拓扑图的时候可以转换成dp
@@ -6,7 +30,7 @@
 using namespace std;
 
 const int N = 11;
-int g[N][N], dp[N * 2][N][N];  // dp[k][i1][i2]代表，时间点k 路线1横坐标i1 路线2横坐标i2 时，两个路线能取的最大数
+int g[N][N], dp[N * 2][N][N];  // dp[k][i1][i2]代表，时间点k 路线1横坐标i1 路线2横坐标i2 时，两个路线取值之和的最大值
 int n;
 
 int main(){
@@ -14,8 +38,8 @@ int main(){
     int a, b, c;
     while(cin >> a >> b >> c, a || b || c) g[a][b] = c;
     for(int k = 2; k <= n + n; k++){  // k = i1 + j1 = i2 + j2, 或者说k代表每个时间步
-        for(int i1 = 1; i1 < k; i1++){
-            for(int i2 = 1; i2 < k; i2++){
+        for(int i1 = 1; i1 < k && i1 <= n; i1++){
+            for(int i2 = 1; i2 < k && i2 <= n; i2++){
                 int j1 = k - i1, j2 = k - i2;
                 int t = g[i1][j1];
                 if(i1 != i2) t += g[i2][j2];
