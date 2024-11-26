048d915ec1ed2701bcba32b86b150aa553aab74b
powerpuffpomelo <powerpuffpomelo@gmail.com>
2024-11-26 20:20:41 +0800

    update

--- acwing/算法提高课/第1章 动态规划/最长上升子序列模型/272. 最长公共上升子序列.cpp
+++ acwing/算法提高课/第1章 动态规划/最长上升子序列模型/272. 最长公共上升子序列.cpp
@@ -24,3 +24,29 @@ int main(){
     cout << ans << endl;
     return 0;
 }
+
+// ###################################################### 版本2 ###################################################### //
+// 一维滚动数组
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 3010;
+int n, a[N], b[N];
+int dp[N];
+int ans;
+
+int main(){
+    cin >> n;
+    for(int i = 1; i <= n; i++) cin >> a[i];
+    for(int i = 1; i <= n; i++) cin >> b[i];
+    for(int i = 1; i <= n; i++){
+        int maxv = 1;
+        for(int j = 1; j <= n; j++){
+            if(a[i] == b[j]) dp[j] = max(dp[j], maxv);
+            if(a[i] > b[j]) maxv = max(maxv, dp[j] + 1);
+        }
+    }
+    for(int j = 1; j <= n; j++) ans = max(ans, dp[j]);
+    cout << ans << endl;
+    return 0;
+}
