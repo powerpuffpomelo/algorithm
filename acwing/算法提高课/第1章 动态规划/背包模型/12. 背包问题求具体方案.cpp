06475853495129b6e862d4c7571c0c022096ebc4
mello <powerpuffpomelo@gmail.com>
2025-02-17 15:24:53 +0800

    update

--- acwing/算法提高课/第1章 动态规划/背包模型/12. 背包问题求具体方案.cpp
+++ acwing/算法提高课/第1章 动态规划/背包模型/12. 背包问题求具体方案.cpp
@@ -1,25 +1,26 @@
 // ###################################################### 版本1 ###################################################### //
-// 
+// 关于为什么一开始要倒序背包：如果145和135都是最优方案的话，那么如果从后往前遍历（从dp[n][m]往前遍历）
+// 选择5后不知道应该选3还是4，只能看到局部字典序最小，不能看到全局字典序最小。
 #include <bits/stdc++.h>
 using namespace std;
 
 const int N = 1010;
 int dp[N][N], v[N], w[N];
-int n, V;
+int n, m;
 
 int main(){
-    cin >> n >> V;
+    cin >> n >> m;
     for(int i = 1; i <= n; i++) cin >> v[i] >> w[i];
 
     for(int i = n; i >= 1; i--){    // 倒序背包
-        for(int j = 1; j <= V; j++){
+        for(int j = 1; j <= m; j++){
             dp[i][j] = dp[i + 1][j];
             if(j >= v[i]) dp[i][j] = max(dp[i][j], dp[i + 1][j - v[i]] + w[i]);
         }
     }
     // 现在dp[1][V] 是最大价值
 
-    int j = V;
+    int j = m;
     for(int i = 1; i <= n; i++){
         if(j >= v[i] && dp[i][j] == dp[i + 1][j - v[i]] + w[i]){
             cout << i << ' ';
