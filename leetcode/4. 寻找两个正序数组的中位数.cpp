e0fdcf5fa48873753d00dd8d1225024f3b2b8457
mello <powerpuffpomelo@gmail.com>
2025-02-23 20:32:35 +0800

    update

--- leetcode/4. 寻找两个正序数组的中位数.cpp
+++ leetcode/4. 寻找两个正序数组的中位数.cpp
@@ -3,7 +3,7 @@
 class Solution {
 public:
     int find_kth_sorted_arrays(vector<int>& nums1, int i, vector<int>& nums2, int j, int k){
-        if(nums1.size() - i > nums2.size() - j) return find_kth_sorted_arrays(nums2, j, nums1, i, k);  
+        if(nums1.size() - i > nums2.size() - j) return find_kth_sorted_arrays(nums2, j, nums1, i, k);  // 短的放前面
         if(i == nums1.size()) return nums2[j + k - 1];
         if(k == 1) return min(nums1[i], nums2[j]);
         int si = min(i + k / 2, (int)nums1.size()), sj = j + k - (si - i);
@@ -23,3 +23,29 @@ public:
         }
     }
 };
+
+// ###################################################### 版本2 ###################################################### //
+// 无需根据数组长度调序
+class Solution {
+    public:
+        int findTopKNum(vector<int>& nums1, vector<int>& nums2, int k, int vec_1_cur, int vec_2_cur) {
+            if (nums1.size() == vec_1_cur) return nums2[vec_2_cur + k - 1];
+            if (nums2.size() == vec_2_cur) return nums1[vec_1_cur + k - 1];
+    
+            if (k == 1) return min(nums1[vec_1_cur], nums2[vec_2_cur]);
+    
+            int add_pos = k/2 - 1;
+            int vec_1_pos = min((int)nums1.size() - 1, vec_1_cur + add_pos);
+            int vec_2_pos = min((int)nums2.size() - 1, vec_2_cur + add_pos);
+    
+            if (nums1[vec_1_pos] < nums2[vec_2_pos]) return findTopKNum(nums1, nums2, k - (vec_1_pos - vec_1_cur + 1), vec_1_pos + 1, vec_2_cur);
+            
+            return findTopKNum(nums1, nums2, k - (vec_2_pos - vec_2_cur + 1), vec_1_cur, vec_2_pos + 1);
+        }
+    
+        double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+            if ((nums1.size() + nums2.size()) % 2 == 1) return findTopKNum(nums1, nums2, (nums1.size() + nums2.size()) / 2 + 1, 0, 0);
+    
+            return (findTopKNum(nums1, nums2, (nums1.size() + nums2.size()) / 2, 0, 0) + findTopKNum(nums1, nums2, (nums1.size() + nums2.size()) / 2 + 1, 0, 0)) / 2.0;
+        }
+    };
