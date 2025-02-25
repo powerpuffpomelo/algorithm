8c6ee72c81c594fed58527265f358b6152a2a989
mello <powerpuffpomelo@gmail.com>
2025-02-25 11:15:17 +0800

    update

---
+++ acwing/算法提高课/第1章 动态规划/背包模型/11. 背包问题求方案数.cpp
@@ -0,0 +1,29 @@
+// ###################################################### 版本1 ###################################################### //
+// 优化成一维
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1010, mod = 1e9 + 7;
+int n, m, dp[N], cnt[N]; 
+// dp[i][j]代表使用前i种物品，总体积不超过j的最大价值
+// cnt[i][j] 代表 dp[i][j] 对应的方案数
+
+int main(){
+    cin >> n >> m;
+    for(int i = 0; i <= m; i++) cnt[i] = 1; // 不超过任何体积的背包 都可以什么也不选，就是一种方案
+    for(int i = 0; i < n; i++){
+        int v, w;
+        cin >> v >> w;
+        for(int j = m; j >= v; j--){
+            int maxw = max(dp[j], dp[j - v] + w);
+            int s = 0;
+            if(dp[j] == maxw) s += cnt[j];
+            if(dp[j - v] + w == maxw) s += cnt[j - v];
+            s %= mod;
+            dp[j] = maxw;
+            cnt[j] = s;
+        }
+    }
+    cout << cnt[m] << endl;
+    return 0;
+}
