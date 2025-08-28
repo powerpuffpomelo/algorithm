46d3d82145f13a45520939e71d054e818c2e1881
mello <powerpuffpomelo@gmail.com>
2025-08-28 11:03:29 +0800

    update

--- 碎片/两种打分的差异.cpp
+++ 碎片/两种打分的差异.cpp
@@ -10,29 +10,28 @@ int n;
 pii a[N], b[N];  // 输入的两种打分
 map<int, int> mp; // 映射，从id值到位置
 int q[N], temp[N];  // 求逆序对的数组，以及temp数组
-LL ans=0;      // ans就是最终返回的逆序对
 
 bool cmp(pii x, pii y){
     return x.first > y.first;
 }
 
-// 直接操作全局变量ans
-void merge_sort(int q[], int l, int r){
-    if(l >= r) return;
+// 归并排序
+LL merge_sort(int q[], int l, int r){
+    if(l >= r) return 0;
     int mid = l + r >> 1;
-    merge_sort(q, l, mid);
-    merge_sort(q, mid+1, r);
-    int i = l, k = l, j = mid + 1;
+    LL ans = merge_sort(q, l, mid) + merge_sort(q, mid + 1, r);
+    int i = l, j = mid + 1, k = l;
     while(i <= mid && j <= r){
         if(q[i] <= q[j]) temp[k++] = q[i++];
         else{
-            ans += (mid - i + 1); 
             temp[k++] = q[j++];
-        }
+            ans += mid - i + 1;
+        } 
     }
-    while(i<=mid) temp[k++] = q[i++];
-    while(j<=r) temp[k++] = q[j++];
-    for(int i=l;i<=r;i++) q[i] = temp[i];
+    while(i <= mid) temp[k++] = q[i++];
+    while(j <= r) temp[k++] = q[j++];
+    for(int i = l; i <= r; i++) q[i] = temp[i];
+    return ans;
 }
 
 int main(){
@@ -51,7 +50,7 @@ int main(){
         q[i] = mp[b[i].second];  // 取出位置，用于计算逆序对
     }
     
-    merge_sort(q, 0, n-1);
+    LL ans = merge_sort(q, 0, n-1);
     cout << ans << endl;
     return 0;
 }
